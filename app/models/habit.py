696fcad57fa5c99f7c2f920b74280c8f4a39dce2
Abel Tesfa <abeltesfa@gmail.com>
2023-02-24 14:38:22 -0500

    habit table added

---
+++ app/models/habit.py
@@ -0,0 +1,13 @@
+from .db import db
+
+
+class Habit(db.Model):
+    __tablename__ = 'habits'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    name = db.Column(db.String(500), nullable=False)
+    frequency = db.Column(db.Integer, nullable=False)
+    daily = db.Column(db.Boolean, nullable=False)
+
+    habit_owner = db.relationship('User', back_populates='user_habits')
