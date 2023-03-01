ba7abb2395a7821e70cf72705c49e077a64b59f4
Abel Tesfa <abeltesfa@gmail.com>
2023-03-01 17:51:12 -0500

    user edit working with errors in nav

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import UserEditForm from './UserEditForm';
 
 const NavBar = () => {
   return (
@@ -26,6 +27,9 @@ const NavBar = () => {
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
+        </li>
+        <li>
+          <UserEditForm />
         </li>
         <li>
           <LogoutButton />
