bea758d6cfc8a76a85bbcd9e83a28cf591653004
Eddie <Edwinjlau26@gmail.com>
2023-03-03 11:40:01 -0700

    quick edit

--- react-app/src/components/Navigation/HomePage.js
+++ react-app/src/components/Navigation/HomePage.js
@@ -1,5 +1,7 @@
 import SideBar from "./SideBar";
 import './HomePage.css'
+import AllHabits from "../Habit/AllHabits";
+import AddHabit from "../Habit/AddHabitModal";
 
 const HomePage = () => {
 
@@ -12,6 +14,9 @@ const HomePage = () => {
                 </div>
                 <div id="main-container">
                     <h3>MAIN CONTENT</h3>
+                    <AllHabits/>
+                    <AddHabit/>
+
                 </div>
                 <div id="misc-container">
                     <h3>MISC STATS, ETC</h3>
