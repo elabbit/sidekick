606d796eeb6017396e3adeea7e7aa6169c7edd50
Abel Tesfa <abeltesfa@gmail.com>
2023-03-05 15:28:18 -0500

    edit habit modal props

--- react-app/src/components/Habit/EditHabitModal.js
+++ react-app/src/components/Habit/EditHabitModal.js
@@ -1,3 +1,39 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Modal } from "../../context/Modal";
+
+const EditHabit = ({freq, habitId}) => {
+    const [showModal, setShowModal] = useState(false);
+    const [frequency, setFrequency] = useState(freq);
+
+    const dispatch = useDispatch()
+
+    const handleSubmit = async (e) => {
+
+    }
+
+    const handleCancel = (e) => {
+        e.preventDefault();
+        setShowModal(false);
+    }
+
+    return (
+        <div>
+            <button onClick={() => setShowModal(true)}>Edit Habit</button>
+            {showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <form onSubmit={handleSubmit}>
+                        <h2>Edit habit</h2>
+                        <label>Frequency:</label>
+                        <input type="number" value={frequency}
+                            onChange={(e) => setFrequency(e.target.value)} />
+                        <button type="submit">Edit Habit</button>
+                        <button type='button' onClick={handleCancel}>Cancel</button>
+                    </form>
+                </Modal>
+            )}
+        </div>
+    )
+}
+
+export default EditHabit;
