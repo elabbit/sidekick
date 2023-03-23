d4d8054064a61268d8ea6cea83dc5c9dbad9ed96
Lynn Luong <lynn.h.luong@gmail.com>
2023-03-22 23:05:55 -0400

    thunks for todo lists

--- react-app/src/store/todo.js
+++ react-app/src/store/todo.js
@@ -28,6 +28,66 @@ const actionDeleteList = (listId) => {
     }
 }
 
+export const thunkLoadLists = (userId) => async (dispatch) => {
+    const response = await fetc(`/api/todo/lists/${userId}`);
+
+    if (response.ok) {
+        const lists = await response.json();
+        dispatch(actionLoadLists(lists));
+        return response;
+    } else {
+        return response.json()
+    }
+}
+
+export const thunkCreateList = (listData) => async (dispatch) => {
+    const response = await fetch('/api/todo/lists', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(listData)
+    });
+
+    if (response.ok) {
+        const list = await response.json()
+        dispatch(actionCreateList(list))
+        return list;
+    }
+
+}
+
+export const thunkUpdateList = (listData) => async (dispatch) => {
+    const response = await fetch(`/api/todo/lists/${listData.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id: listData.id, name: listData.name})
+    })
+
+
+    if (response.ok) {
+        const list = await response.json()
+        dispatch(actionUpdateList(list))
+        return list;
+    }
+}
+
+export const thunkDeleteList = (listId) => async (dispatch) =>{
+    const response = await csrfFetch(`/api/todo/lists/${listId}`, {
+        method: 'DELETE',
+    });
+
+    if (response.ok) {
+        const listId = await response.json()
+        dispatch(actionDeleteList(listId))
+        return listId
+    }
+}
+
+
+
 const todoReducer = (state = {}, action) => {
 
 }
