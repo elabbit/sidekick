9f1f2231cad144fa1cb82bd334ebc4ed18229139
Lynn Luong <lynn.h.luong@gmail.com>
2023-03-26 14:43:16 -0400

    added seed commands

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,7 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .habits import seed_habits, undo_habits
-
+from .lists import seed_lists, undo_lists
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
@@ -12,6 +12,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_habits()
+    seed_lists()
     # Add other seed functions here
 
 
@@ -20,4 +21,5 @@ def seed():
 def undo():
     undo_habits()
     undo_users()
+    undo_lists()
     # Add other undo functions here
