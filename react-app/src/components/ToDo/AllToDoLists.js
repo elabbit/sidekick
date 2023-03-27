5d81240f5a2e83cd3e87381a79d70711f17b9b74
Abel Tesfa <abeltesfa@gmail.com>
2023-03-27 19:54:52 -0400

    lists names displaying

--- react-app/src/components/ToDo/AllToDoLists.js
+++ react-app/src/components/ToDo/AllToDoLists.js
@@ -5,6 +5,8 @@ import { loadLists } from '../../store/todo';
 const AllToDoLists = () => {
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
+    const lists = useSelector(state => state.todolists)
+    const listsArr = lists ? Object.values(lists) : null;
 
     useEffect(() => {
         const loadToDoLists = async () => {
@@ -15,7 +17,16 @@ const AllToDoLists = () => {
 
     return (
         <div>
-
+            <h2>ToDo Lists</h2>
+            {listsArr && listsArr.map((list) => {
+                return(
+                    <div key={list.id}>
+                        {list.name}
+                    </div>
+                )
+            })}
+            <div>
+            </div>
         </div>
     )
 }
