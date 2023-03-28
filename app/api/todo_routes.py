bb436ef1305d870d1b100f2d425756aaf22746d2
Abel Tesfa <abeltesfa@gmail.com>
2023-03-28 12:03:53 -0400

    create todolist route with error function added

--- app/api/todo_routes.py
+++ app/api/todo_routes.py
@@ -1,7 +1,19 @@
 from flask import Blueprint, request
 from app.models import TodoList, db
+from app.forms import ToDoListForm
 
 todo_routes = Blueprint('todo', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{error}')
+            # errorMessages.append(f'{field} : {error}')
+    return errorMessages
 
 @todo_routes.route('/lists/<int:userId>')
 def todo_lists(userId):
@@ -10,7 +22,19 @@ def todo_lists(userId):
 
 @todo_routes.route('/lists', methods=['POST'])
 def create_todo_lists():
-    pass
+    form = ToDoListForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        todolist = TodoList(
+            user_id=form.data['user_id'],
+            name=form.data['name']
+        )
+        db.session.add(todolist)
+        db.session.commit()
+        return todolist.to_dict()
+
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
 
 @todo_routes.route('/<int:listId>/tasks')
 def todo_tasks(listId):
