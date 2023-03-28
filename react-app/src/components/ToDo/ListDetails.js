bfef216b97cfe8a4d1c070f2df440aba5ed4db3d
Abel Tesfa <abeltesfa@gmail.com>
2023-03-28 14:47:28 -0400

    delete list component modal added

--- react-app/src/components/ToDo/ListDetails.js
+++ react-app/src/components/ToDo/ListDetails.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import DeleteToDoList from "./DeleteToDoListModal";
 import EditToDoList from './EditToDoList';
 
 const ListDetails = ({ list }) => {
@@ -10,6 +11,7 @@ const ListDetails = ({ list }) => {
                 <div>
                     {list.name}
                     <button onClick={() => setDisplayEditListForm(true)}>Edit List Name</button>
+                    <DeleteToDoList list={list}/>
                 </div>
                 : <EditToDoList list={list} hideForm={() => setDisplayEditListForm(false)} />
             }
